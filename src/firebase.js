95c276fda1b371dc3f4c141adf490966ecca2a37
Petra Puljić <ppuljicc@gmail.com>
2024-08-09 11:18:51 +0200

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDSRZyMwBlqq-5iHQ2lQdq5TwhUAew9f70",
+    authDomain: "twirltrack-a628e.firebaseapp.com",
+    projectId: "twirltrack-a628e",
+    storageBucket: "twirltrack-a628e.appspot.com",
+    messagingSenderId: "316986222980",
+    appId: "1:316986222980:web:0b38442d36126745e415d1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export default {
+    app,
+    auth,
+    firestore,
+};
